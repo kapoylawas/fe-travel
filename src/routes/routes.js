680d88f653b9a34fb9f188c4aab7ea3921e79a38
arief sangga <arif.sangga@gmail.com>
2022-07-20 14:54:11 +0700

    update components, layouts, dahsboard, privateroutes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,9 @@
 //import react router dom
 import { Switch, Route } from "react-router-dom";
 
+//import component private routes
+import PrivateRoute from "./PrivateRoutes";
+
 //=======================================================================
 //ADMIN
 //=======================================================================
@@ -8,14 +11,22 @@ import { Switch, Route } from "react-router-dom";
 //import view Login
 import Login from '../pages/admin/Login';
 
+//import view admin Dashboard
+import Dashboard from '../pages/admin/dashboard/Index';
+
 function Routes() {
     return (
         <Switch>
 
-            {/* route "/admin/login" */}
+            {/* route "/adminlogin" */}
             <Route exact path="/admin/login">
                 <Login /> 
             </Route>
+
+            {/* private route "/admin/dashboard" */}
+            <PrivateRoute exact path="/admin/dashboard">
+                <Dashboard /> 
+            </PrivateRoute>
 
         </Switch>
     )
